6062198180af2165f7a11af3a3dc44dc1cb44fb7
Nimor <e-urezu@mail.ru>
2025-01-28 10:55:49 +0300

    p101

--- p10/main.go
+++ p10/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() {
@@ -17,15 +18,36 @@ func UniqueUserIDs(userIDs []int64) []int64 {
 	if len(userIDs) < 2 {
 		return userIDs
 	}
-	resSlice := make([]int64, 0, 5)
+	userIDs = slices.Compact(userIDs)
+	resSlice := make([]int64, 0, len(userIDs))
+	resSlice = append(resSlice, userIDs[0])
 	for i := 0; i < len(userIDs); i++ {
-		resSlice = append(resSlice, userIDs[i])
-		for j := 1; j < len(userIDs); j++ {
-			if resSlice[i] == userIDs[j] {
-				userIDs = append(userIDs[:j], userIDs[j+1:]...) //a = append(a[:1], a[2:]...)
-				j--
-			}
+		if slices.Contains(userIDs, resSlice[i]) {
+			continue
+		} else {
+			resSlice = append(resSlice, userIDs[i])
 		}
 	}
+
+	// for j := 1; j < len(userIDs); j++ {
+	// 	if userIDs[j-1] == userIDs[j] {
+	// 		userIDs = append(userIDs[:j], userIDs[j+1:]...) //a = append(a[:1], a[2:]...)
+	// 	}
+	// 	if userIDs[j-1] != userIDs[j] {
+	// 		resSlice = append(resSlice, userIDs[j])
+
+	// 	}
+	// }
 	return resSlice
+	// resSlice := make([]int64, 0, 5)
+	// for i := 0; i < len(userIDs); i++ {
+	// 	resSlice = append(resSlice, userIDs[i])
+	// 	for j := 1; j < len(userIDs); j++ {
+	// 		if resSlice[i] == userIDs[j] {
+	// 			userIDs = append(userIDs[:j], userIDs[j+1:]...) //a = append(a[:1], a[2:]...)
+	// 			j--
+	// 		}
+	// 	}
+	// }
+	// return resSlice
 }
