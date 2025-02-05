d12a2530e7a4054ec0860ab795f3fe2c3a5a12bc
Nimor <e-urezu@mail.ru>
2025-02-05 19:43:54 +0300

    12

--- p12/main.go
+++ p12/main.go
@@ -4,14 +4,20 @@ import "fmt"
 
 func main() {
 	//fmt.Println(byte('a'))
-	fmt.Println(ShiftASCII("abc", 0))
-	fmt.Println(ShiftASCII("abc1", 1))
-	fmt.Println(ShiftASCII("bcd2", -1))
-	fmt.Println(ShiftASCII("hi", 10))
-	fmt.Println(ShiftASCII("abc", 256))
-	fmt.Println(ShiftASCII("abc", -511))
+	// fmt.Println(ShiftASCII("abc", 0))
+	// fmt.Println(ShiftASCII("abc1", 1))
+	// fmt.Println(ShiftASCII("bcd2", -1))
+	// fmt.Println(ShiftASCII("hi", 10))
+	// fmt.Println(ShiftASCII("abc", 256))
+	// fmt.Println(ShiftASCII("abc", -511))
+	fmt.Println("Hello helix")
+	PrintSum(28, 35)
+	PrintSum(45, 28)
 }
 
+func PrintSum(n1 int, n2 int) {
+	fmt.Println(n1 + n2)
+}
 func ShiftASCII(s string, step int) string {
 	var rs string
 	for _, b := range s {
