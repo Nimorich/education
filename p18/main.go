a665fddd0eda929843f8377d5bd42eee61e32579
Nimorich <Nimorich@ya.ru>
2025-02-07 14:57:57 +0300

    21

--- p18/main.go
+++ p18/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+type Creature struct{
+	Species string
 }
+
+func (c Creature) Reset(){
+	c.Species = ""
+}
+
+func main() {
+	var creature Creature = Creature{Species: "shark"}
+	fmt.Println("Привет")
+	fmt.Printf("1) %+v\n", creature)
+	creature.Reset()
+	fmt.Printf("2) %+v\n", creature)
+}
+
+// type Creature struct {
+// 	Species string
+// }
+
+// func (c Creature) String() string {
+// 	return c.Species
+// }
