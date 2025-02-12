d572aa794def081b2fdf87436d46878ef4d0d147
Nimor <e-urezu@mail.ru>
2025-02-12 22:43:54 +0300

    p22

--- p22/main.go
+++ p22/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "fmt"
+
+type Grades []float64
+
+func (g *Grades) add(grade float64) {
+	*g = append(*g, grade)
+}
+
+func main() {
+	grades := Grades{3.7, 4.2, 5.0}
+	(&grades).add(4.8)
+	fmt.Println(grades) // [3.7 4.2 5 4.8]
+}
