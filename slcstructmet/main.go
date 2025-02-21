66f1aba71f7129cf7291c6cd789aed8104a8c658
Nimorich <Nimorich@ya.ru>
2025-02-22 02:56:44 +0300

    Slice Struct and Metod

---
+++ slcstructmet/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	employees := PersonList{{Age: 18}, {Age: 44}, {Age: 18},}
+	employees.GetAgePopularity()
+}
+
+type Person struct {
+	Age uint8
+}
+type PersonList []Person
+
+func (pl PersonList) GetAgePopularity() map[uint8]int {
+	plm := make(map[uint8]int)
+	for _, v := range pl {
+		plm[v.Age]++
+	}
+	return plm
+
+}
+
+
