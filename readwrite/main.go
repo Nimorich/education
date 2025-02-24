d972ac2d6f7faa099e7a3666967b9208b09128bd
Nimorich <Nimorich@ya.ru>
2025-02-25 02:12:22 +0300

    readwrite

---
+++ readwrite/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	file := createFile()
+	writeFile(&file)
+	defer file.Close()
+}
+func readFile() {
+	file, err := os.Open("hello.txt")
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	data := make([]byte, 64)
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF {
+			break
+		}
+		fmt.Print(string(data[:n]))
+	}
+	defer file.Close()
+}
+func writeFile(file *os.File) {
+	date := ""
+	name := ""
+	fmt.Print("На какое число вы хотите забронировать номер?: ")
+	fmt.Scan(&date)
+	fmt.Print("Ваше имя: ")
+	fmt.Scan(&name)
+	file.WriteString(date + " " + name + "\n")
+	fmt.Println("................................................")
+	fmt.Printf("Поздравляю %s. Номер забронирован на: %s\n", name, date)
+}
+func createFile() os.File {
+	_, err := os.Stat("hello.txt")
+	if err != nil {
+		file, err := os.Create("hello.txt")
+		if err != nil {
+			fmt.Println("Unable to create file:", err)
+			os.Exit(1)
+		}
+		return *file
+	} else {
+		fileStat, err := os.OpenFile("hello.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		return *fileStat
+	}
+
+}
