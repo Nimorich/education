a1f330b4059d245ea22bc910589345e70b55ff2d
Nimor <e-urezu@mail.ru>
2025-11-04 14:27:22 +0300

    modify

---
+++ RustProjects/lifetimes/target/debug/deps/lifetimes-4e24f30400ea86ef.d
@@ -0,0 +1,5 @@
+D:\Work\education\RustProjects\lifetimes\target\debug\deps\lifetimes-4e24f30400ea86ef.d: src\main.rs
+
+D:\Work\education\RustProjects\lifetimes\target\debug\deps\liblifetimes-4e24f30400ea86ef.rmeta: src\main.rs
+
+src\main.rs:
---
+++ RustProjects/lifetimes/target/debug/deps/lifetimes-eb4d1cb936d63d4d.d
@@ -0,0 +1,5 @@
+D:\Work\education\RustProjects\lifetimes\target\debug\deps\lifetimes-eb4d1cb936d63d4d.d: src\main.rs
+
+D:\Work\education\RustProjects\lifetimes\target\debug\deps\liblifetimes-eb4d1cb936d63d4d.rmeta: src\main.rs
+
+src\main.rs:
